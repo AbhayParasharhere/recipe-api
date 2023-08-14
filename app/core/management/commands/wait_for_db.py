799c3c8aa6f9f24616823d723366b6d08a7ae269
Abhay <abhayparasharr@gmail.com>
2023-08-13 20:22:52 -0700

    Fix linting issues and add wait_for_db command

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,6 +1,12 @@
 """
 Django command to wait for the database to be available
 """
+import time
+
+from psycopg2 import OperationalError as Psycopg2Error
+
+from django.db import OperationalError
+
 from django.core.management.base import BaseCommand
 
 
@@ -8,5 +14,15 @@ class Command(BaseCommand):
     """Django command to wait for database"""
 
     def handle(self, *args, **options):
-        print('Waiting for database...')
-        pass
+        self.stdout.write('Waiting for database...')
+        db_ready = False
+
+        while db_ready is False:
+            try:
+                self.check(databases=['default'])
+                db_ready = True
+            except (Psycopg2Error, OperationalError):
+                self.stdout.write('Database unavailable, waiting 1 second ...')
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('Database available'))
